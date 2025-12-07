eed632bae46c375543e468ef956c9d1644fa2152
dreamgamer5000 <antonyrejit@gmail.com>
2025-12-07 20:08:53 +0530

    Blog website complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://dream-first-astro-project.netlify.app"
+  site: "https://dream-first-astro-project.netlify.app",
+  integrations: [preact()]
 });
